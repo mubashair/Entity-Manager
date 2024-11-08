127bb8b81bad790458c034e6410e776281793490
Mubashar <mubashair@gmail.com>
2024-11-08 07:01:07 +0500

    model is created

--- src/main/java/com/entityManager/model/Employee.java
+++ src/main/java/com/entityManager/model/Employee.java
@@ -1,6 +1,53 @@
 package com.entityManager.model;
 
+import jakarta.persistence.Column;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "employees")
 public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
 	private Long id;
+	@Column(name = "emp_name")
+	
+	private String name;
+	@Column(name = "emp_position")
+	private String position;
+	
+	@Column(name = "emp_salary")
+	private Double salary;
+	
+	//Getters and Setters 
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPosition() {
+		return position;
+	}
+	public void setPosition(String position) {
+		this.position = position;
+	}
+	public Double getSalary() {
+		return salary;
+	}
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
 
 }
