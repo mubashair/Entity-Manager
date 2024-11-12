7e2f31126d8d4f025a3571a650054ee01aa94326
Mubashar <mubashair@gmail.com>
2024-11-12 09:04:10 +0500

    edit feature added

--- src/main/java/com/entityManager/EmployeeController/EmployeeController.java
+++ src/main/java/com/entityManager/EmployeeController/EmployeeController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -25,7 +26,7 @@ public class EmployeeController {
 	// Display the form to create a new employee
 	@GetMapping("/new")
 	public String  newEmployeeForm(Model model) {
-		model.addAttribute("employee", new Employee());
+		model.addAttribute("emp", new Employee());
 		return "employee_form";
 	}
 	//Handle form submission for creating or updating an employee
@@ -33,6 +34,14 @@ public class EmployeeController {
 	public String saveEmployee(@ModelAttribute("employee") Employee employee) {
 		employeeService.saveEmployee(employee);
 		return "redirect:/employees";
+		
+	}
+	//Display the form to edit an existing employee
+	@GetMapping("/edit/{id}")
+	public String editEmployeeForm(@PathVariable("id") Long id, Model model) {
+		model.addAttribute("emp", employeeService.getEmployeeById(id));
+		
+		return "employee_form";
 		
 	}
 	
