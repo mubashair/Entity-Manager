eb38c2996ef0f260e743da8a3ea923b3044a9927
Mubashar <mubashair@gmail.com>
2024-11-22 09:25:21 +0500

    error.html updated

--- src/main/java/com/entityManager/exception/GlobalExceptionHandler.java
+++ src/main/java/com/entityManager/exception/GlobalExceptionHandler.java
@@ -1,14 +1,32 @@
 package com.entityManager.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+	//Initializes a logger for this class to record exception details and debugging information.
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+    
+	//Handle EmployeeNotFound exception
 	@ExceptionHandler(EmployeeNotFoundException.class)
 	public String employeeNotFoundException(EmployeeNotFoundException employeeNotFoundException, Model model) {
 		model.addAttribute("errorMessage", employeeNotFoundException.getMessage());
 		return "error";
+	}
+	//Handle UserNotFoundException
+	@ExceptionHandler(UsernameNotFoundException.class)
+	public String handleUserNotFoundException(UsernameNotFoundException exception, WebRequest request, Model model) {
+		logger.error("Error: {}", exception.getMessage());
+		model.addAttribute("error", "User Not Found!");
+		model.addAttribute("message", exception.getMessage());
+		model.addAttribute("details", request.getDescription(false));
+		model.addAttribute("timestamp", System.currentTimeMillis());
+		return "error";//Thymeleaf template name
 	}
 }
